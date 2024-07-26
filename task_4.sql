083be0a53c8bee39300ad8252f68bb58c5a92b9b
Ouko Okowa <oukopinto@gmail.com>
2024-07-26 18:03:33 +0200

    Added task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -3,7 +3,7 @@ USE alx_book_store;
 
 --@block
 --# Shows full description of Books table in alx_book_store
-SELECT COLUMN_NAME, DATA_TYPE 
+SELECT COLUMN_NAME, COLUMN_TYPE, DATA_TYPE 
 FROM INFORMATION_SCHEMA.COLUMNS 
 WHERE TABLE_SCHEMA = 'alx_book_store' 
 AND TABLE_NAME = 'Books';
