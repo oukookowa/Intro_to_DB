3aa839ca3b1f4fbd773645bbc687101ca2df4d95
Ouko Okowa <oukopinto@gmail.com>
2024-07-26 18:45:37 +0200

    Added task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,9 +1,18 @@
 --@block
 USE alx_book_store;
 
+
 --@block
-INSERT INTO customers
+CREATE TABLE IF NOT EXISTS customer(
+    customer_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_name VARCHAR(215),
+    email VARCHAR(215) UNIQUE,
+    address TEXT
+);
+
+--@block
+INSERT INTO customer
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave');
 
 --@block
-SELECT * FROM customers;
+SELECT * FROM customer;
